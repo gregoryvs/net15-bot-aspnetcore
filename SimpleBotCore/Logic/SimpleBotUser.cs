14c54715601a311b0cf7bc3e2b4e2a8179219c05
gregoryvs <gregoryvictorelli@gmail.com>
2019-06-05 21:21:21 -0300

    Implementacao Mongo

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace SimpleBotCore.Logic
 {
@@ -9,6 +11,17 @@ namespace SimpleBotCore.Logic
     {
         public string Reply(SimpleMessage message)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("15net");
+            var col = db.GetCollection<BsonDocument>("col01");
+
+            var doc = new BsonDocument() {
+                { "user", message.User },
+                {"mensagem", message.Text }
+            };
+
+            col.InsertOne(doc);
+
             return $"{message.User} disse '{message.Text}'";
         }
 
